fa9d5744bf17d234ba1342dea16a095afb54346c
Ondrej Theiner <theon@seznam.cz>
2025-09-08 01:41:06 +0200

    Fixing multiple galleries on the page

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,10 +1,12 @@
 // Wait until the page is fully loaded
 document.addEventListener("DOMContentLoaded", function() {
   // Initialize Justified Gallery
-  $('#myGallery').justifiedGallery({
-    rowHeight : 150,
-    lastRow : 'center',
-    margins : 5
+  $('[id^=gallery-]').each(function() {
+    $(this).justifiedGallery({
+      rowHeight : 150,
+      lastRow : 'center',
+      margins : 5
+    });
   });
 
   // Responsive adjustment (optional)
