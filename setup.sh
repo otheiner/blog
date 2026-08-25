6ef8cf552e61b683fb906be872d8a522ad857642
Ondrej Theiner <theon@seznam.cz>
2026-08-26 00:05:48 +0200

    To the sea part finished

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
+echo "*************************************************************"
+echo "* Test page locally. To deploy, go to Actions tab in GitHub *"
+echo "* and trigger the workflow manually.                        *"
+echo "*************************************************************\n"
+
 bundle exec jekyll serve
